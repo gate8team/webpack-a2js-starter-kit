355b161fbeaea26cc365383c8b8cd5343d00a8e1
anton.shabanov <anton.shabanov@castle.co>
2016-08-09 17:58:48 +0400

    Moved component to another directory.

--- src/components/app/app.component.ts
+++ src/components/app/app.component.ts
@@ -1,8 +1,14 @@
 import { Component } from '@angular/core';
-import '../../public/css/styles.css';
+import '../../../public/css/styles.css';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent { }
+export class AppComponent {
+  name: string;
+
+  constructor() {
+    this.name = 'Anton';
+  }
+}
